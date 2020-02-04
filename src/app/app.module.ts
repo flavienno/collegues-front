ab4a7f4256867beab9c0cd863cb8c568116042c9
Flavien Oulhiou-Naudin <flavien.naudin@gmail.com>
2020-02-04 11:02:16 +0100

    TP 2 V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { CollegueComponent } from './collegue/collegue.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollegueComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/Collegue.ts
@@ -0,0 +1,24 @@
+
+export class Collegue {
+
+    matricule: string;
+    nom: string;
+    prenoms: string;
+    email: string;
+    dateDeNaissance: Date;
+    photoUrl: string;
+
+
+
+    constructor(_matricule: string,
+        _nom: string, _prenoms: string, _email: string, _dateDeNaissance: Date, _photoUrl: string) {
+        this.matricule = _matricule;
+        this.nom = _nom;
+        this.email = _email;
+        this.dateDeNaissance = _dateDeNaissance;
+        this.photoUrl = _photoUrl;
+
+    }
+
+
+}
