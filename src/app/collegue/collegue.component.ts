40ab3f065fc0b30e12c6e1055ee8fb75ef040fcd
Flavien Oulhiou-Naudin <flavien.naudin@gmail.com>
2020-02-04 21:48:24 +0100

    TP 03 en cours

--- src/app/collegue/collegue.component.ts
+++ src/app/collegue/collegue.component.ts
@@ -9,15 +9,31 @@ import { Collegue } from '../models/Collegue';
 export class CollegueComponent implements OnInit {
   @Input() col: Collegue;
 
+  modeEdition = false;
+
+  emailSaisie: string;
+  photoSaisie: string;
+
   constructor() { }
 
   modifierCollegue(){
 
+    this.modeEdition = true;
     console.log('Modification du collègue');
   }
 
   creerCollegue() {
     console.log('Création d’un nouveau collègue');
+  }
+  emailChange(valeurSaisie: string) {
+    this.emailSaisie = valeurSaisie;
+  }
+  photoChange(adresseSaisie: string){
+    this.photoSaisie = adresseSaisie;
+  }
+
+  validerLEdition(){
+    console.log('Validation du collègue');
   }
 
   ngOnInit() {
